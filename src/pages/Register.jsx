225b668c09a516528262cde2ca17e5e9b3bc239b
OunifiSamirIT <ounifisamiritdsi@gmail.com>
2024-04-03 16:44:24 +0100

    url base change

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5285,7 +5285,7 @@ function Register() {
     formDataToSubmit.append("file", File || null);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://odine-sport.com/api/auth/signup", {
         method: "POST",
         body: formDataToSubmit,
       });
