5f935fa042d04cd5d8601fff5d0cff77f264e3d1
Nader <ghazouani.nader@gmail.com>
2024-04-17 17:39:57 +0100

    minor changes

--- src/config.js
+++ src/config.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 export const Config  = {
 
-    LOCAL_URL :  'http://localhost:5000',
-    // LOCAL_URL :  'https://odine-sport.com',
+    // LOCAL_URL :  'http://localhost:5000',
+    LOCAL_URL :  'https://odine-sport.com',
 }
-=======
-export const Config = {
-  LOCAL_URL: "http://localhost:5000",
-  //  LOCAL_URL :  'https://odine-sport.com',
-};
->>>>>>> e101a6e737c13659282a19fcb52b9db0d70e3ea2
