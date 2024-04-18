93ad37160e86e12bb43179b8e4dbfb96bb010183
ih_____________eb <ihebloufi@gmail.com>
2024-04-18 12:07:27 +0100

    fixing profile player & others

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
 export const Config  = {
 
-<<<<<<< HEAD
     // LOCAL_URL :  'http://localhost:5000',
     LOCAL_URL :  'https://odinesport.com',
-=======
-    LOCAL_URL :  'http://localhost:5000',
-    // LOCAL_URL :  'https://odine-sport.com',
->>>>>>> c29e4bd (fixing profile player & others)
 }
