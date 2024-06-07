c9fd53d387cc23ac619336ad4ba21904bb7735dd
Dhia-dev <dhia.mejri@esprit.tn>
2024-06-07 17:50:14 +0100

    FIX

--- src/components/SlideMenu.jsx
+++ src/components/SlideMenu.jsx
@@ -1,67 +1,76 @@
 import React, { useState, useEffect } from "react";
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import UserImage from "../assets/placeholder.jpg"
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import UserImage from "../assets/placeholder.jpg";
 import { Config } from "../config";
 import LanguageToggler from "../fixDesignComponents/languageToggler";
 import { Context } from "../index";
 import gsap from "gsap";
 
-const SlideMenu = ({isActive, setPopupNotificationHidden,setIsActive, setHumberger, Hamburger, notificationData, deleteNotData, setnotificationData, NotificationService }) => {
-  const [lang, setLang] = useState('Français')
-  const [user, setUser] = useState({})
+const SlideMenu = ({
+  isActive,
+  setPopupNotificationHidden,
+  setIsActive,
+  setHumberger,
+  Hamburger,
+  notificationData,
+  deleteNotData,
+  setnotificationData,
+  NotificationService,
+}) => {
+  const [lang, setLang] = useState("Français");
+  const [user, setUser] = useState({});
   const storedUserData = JSON.parse(localStorage.getItem("user"));
-  const {_currentLang, _setLang, getTranslation} = React.useContext(Context)
+  const { _currentLang, _setLang, getTranslation } = React.useContext(Context);
 
   useEffect(() => {
     // Replace the API endpoint with your actual endpoint for fetching user data
     fetch(`${Config.LOCAL_URL}/api/user/${storedUserData.id}`)
       .then((response) => response.json())
       .then((userData) => {
-        console.log(user)
-        setUser(userData.user)
+        console.log(user);
+        setUser(userData.user);
       })
       .catch((error) => console.error("Error fetching user data:", error));
-  }, [])
+  }, []);
   const toggleLanguage = () => {
-    if (lang === 'Français') {
-      setLang('English')
+    if (lang === "Français") {
+      setLang("English");
     } else {
-      setLang('Français')
+      setLang("Français");
     }
-  }
+  };
   const toggleExpand = () => {
     setIsActive(!isActive);
+    !isActive && setPopupNotificationHidden(true);
     !isActive &&
-    setPopupNotificationHidden(true)
-   !isActive &&
-   gsap
-   .timeline()
-   .to(".desktopPopUpNotificationCon", {
-     duration: 0.2,
-     // opacity: 0,
-     pointerEvents: "none",
-     y: 0,
-   })
-   .to(".loadingNot .loaderCon .inner", {
-     duration: 0,
-     width: "0%",
-   })
-   .to(".loadingNot .loaderCon .inner", {
-     duration: 0,
-     opacity: 1,
-   })
-   .to(".loadingNot .loaderCon .inner", {
-     duration: 0,
-     display: "block",
-   })
-   .to(".desktopPopUpNotificationCon .showAllnot", {
-     duration: 0,
-     display: "none",
-   })
-   .to(".desktopPopUpNotificationCon .showAllnot", {
-     duration: 0,
-     opacity: 0,
-   })
+      gsap
+        .timeline()
+        .to(".desktopPopUpNotificationCon", {
+          duration: 0.2,
+          // opacity: 0,
+          pointerEvents: "none",
+          y: 0,
+        })
+        .to(".loadingNot .loaderCon .inner", {
+          duration: 0,
+          width: "0%",
+        })
+        .to(".loadingNot .loaderCon .inner", {
+          duration: 0,
+          opacity: 1,
+        })
+        .to(".loadingNot .loaderCon .inner", {
+          duration: 0,
+          display: "block",
+        })
+        .to(".desktopPopUpNotificationCon .showAllnot", {
+          duration: 0,
+          display: "none",
+        })
+        .to(".desktopPopUpNotificationCon .showAllnot", {
+          duration: 0,
+          opacity: 0,
+        });
   };
   const navigate = useNavigate();
   // Logout function
@@ -73,16 +82,16 @@ const SlideMenu = ({isActive, setPopupNotificationHidden,setIsActive, setHumberg
     // Redirect to the login page or another route
     navigate("/login");
   };
+
   return (
     <>
       <div className=" relative flex  justify-end">
-        
         <img
           alt="user iamge"
           loading="lazy"
           onClick={() => setHumberger(!Hamburger)}
           srcSet={user?.image ? user?.image : UserImage}
-          className="md:hidden  self-stretch aspect-square  rounded-full h-[60px] h-[60px] relative pointer"
+          className="md:hidden  self-stretch aspect-square  rounded-full  h-[60px] relative pointer"
         />
         <img
           alt="user iamge"
@@ -91,16 +100,19 @@ const SlideMenu = ({isActive, setPopupNotificationHidden,setIsActive, setHumberg
           srcSet={user?.image ? user?.image : UserImage}
           className="hidden  border-2   md:block self-stretch  aspect-square  rounded-full w-[60px] z-10 relative pointer"
         />
-        <div className={`hidden  gap-y-2 py-2 md:flex absolute  h-[300px] top-16 z-1 flex w-[200px]  flex-column items-center bg-blue-600 rounded-[8px] max-md:flex-wrap  ${isActive ? 'expand  pointer-events-auto' : ' expand pointer-events-none'}`}
+        <div
+          className={`hidden  gap-y-2 py-2 md:flex absolute  h-[300px] top-16 z-1 flex w-[200px]  flex-column items-center bg-blue-600 rounded-[8px] max-md:flex-wrap  ${
+            isActive
+              ? "expand  pointer-events-auto"
+              : " expand pointer-events-none"
+          }`}
           style={{
             transition: ".3s",
-            opacity: isActive ? 1: 0,
-            transform: isActive ? "translateY(0px)" : " translateY(-3px)"
+            opacity: isActive ? 1 : 0,
+            transform: isActive ? "translateY(0px)" : " translateY(-3px)",
           }}
-        >  
-         <div className="absolute translate-y-1 -translate-x-9">
-         
-         </div>
+        >
+          <div className="absolute translate-y-1 -translate-x-9"></div>
           {/* <div className="flex justify-center items-center self-stretch px-2 my-auto bg-white aspect-square h-[31px] rounded-[50px] w-[31px]">
             <img
               onClick={toggleExpand}
@@ -110,59 +122,159 @@ const SlideMenu = ({isActive, setPopupNotificationHidden,setIsActive, setHumberg
             />
           </div> */}
 
-<div className="w-[90%] flex-1 flex justify-center ">
-          <a className="flex gap-2  h-[100%]  my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn" href={'/home'}> 
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/e13bde84fa9b2785556220b4a439b162b51401e76b4b6dbacca76198fd0f64c0?"
-              className="w-4 aspect-square"
-            />
-            { getTranslation(
-            `Home`,  // -----> Englais
-              `Accueil`, //  -----> Francais
-                           )  } </a>
+          {/* <div className="w-[100%]    flex-1 flex justify-center ">
+            <a
+              className="flex gap-2  h-[100%] mr-12  my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn"
+              href={"/home"}
+            >
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/e13bde84fa9b2785556220b4a439b162b51401e76b4b6dbacca76198fd0f64c0?"
+                className="w-4 aspect-square"
+              />
+              {getTranslation(
+                `Home`, // -----> Englais
+                `Accueil` //  -----> Francais
+              )}{" "}
+            </a>
           </div>
-          <div className="w-[90%] flex-1 flex justify-center ">
-          <a className="flex gap-2  h-[100%]  my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn" href={`/profile/${user?.id}`}>
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/fc3698e77abea2c6c7f4158984e72cc84fccffa79005aaacb0a8148c7fc2d112?"
-              className="aspect-[0.75] w-[13px]"
-            />
-            { getTranslation(
-            `Profile`,  // -----> Englais
-              `Profil`, //  -----> Francais
-                           )  } </a>
+          <div className="w-[100%]  flex-1 flex justify-center ">
+            <a
+              className="flex gap-2  h-[100%] mr-[66px]  my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn"
+              href={`/profile/${user?.id}`}
+            >
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/fc3698e77abea2c6c7f4158984e72cc84fccffa79005aaacb0a8148c7fc2d112?"
+                className="aspect-[0.75] w-[13px]"
+              />
+              {getTranslation(
+                `Profile`, // -----> Englais
+                `Profil` //  -----> Francais
+              )}{" "}
+            </a>
           </div>
-          <div className="w-[90%] flex-1 flex justify-center ">
-          <a className="flex gap-2  h-[100%]  my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn" href={'/setting/personal'}>
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/927661d7ec160577838c28e1e039b5d465c8c91f54895e62de4603b16322853d?"
-              className="w-4 aspect-square"
-            />
-            { getTranslation(
-            `Settings`,  // -----> Englais
-              `Paramètres`, //  -----> Francais
-                           )  }</a>
+          <div className="w-[100%]  flex-1 flex justify-center ">
+            <a
+              className="flex gap-2  h-[100%] mr-4 my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn"
+              href={"/setting/personal"}
+            >
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/927661d7ec160577838c28e1e039b5d465c8c91f54895e62de4603b16322853d?"
+                className="w-4 aspect-square"
+              />
+              {getTranslation(
+                `Settings`, // -----> Englais
+                `Paramètres` //  -----> Francais
+              )}
+            </a>
           </div>
-          
-         
-          <div onClick={handleLogout} className="flex gap-2  slideMenuBtn logoutBtn  my-auto text-base font-medium text-white whitespace-nowrap">
+
+          <div
+            onClick={handleLogout}
+            className="flex gap-2  slideMenuBtn logoutBtn  my-auto text-base font-medium text-white whitespace-nowrap"
+          >
             <img
               loading="lazy"
               src="https://cdn.builder.io/api/v1/image/assets/TEMP/7e89f8c7eb51a481e293bc932ceeb5782dea45f8469a1e16a1753ed7be9f6c3e?"
               className="w-4 aspect-square"
             />
-            <div   >{ getTranslation(
-            `Log Out`,  // -----> Englais
-              `Déconnexion`, //  -----> Francais
-                           )  }</div>
-          </div>
-       
+            <div>
+              {getTranslation(
+                `Log Out`, // -----> Englais
+                `Déconnexion` //  -----> Francais
+              )}
+            </div>
+          </div> */}
+
+          <>
+            <style>
+              {`
+        .separator {
+        border-bottom-width: thin;
+          border-bottom: 1px thin white;
+        }
+        .hover-effect:hover {
+          background-color: rgba(255, 255, 255, 0.2); /* Effet de survol blanc */
+        }
+        .logout-hover-effect:hover {
+         background-color: rgb(255, 152, 0); /* Effet de survol orange (500) */
+        }
+      `}
+            </style>
+
+            <div className="w-[100%] flex-1 flex justify-center separator hover-effect">
+              <a
+                className="flex gap-2 h-[100%] mr-12 my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn"
+                href={"/home"}
+              >
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/e13bde84fa9b2785556220b4a439b162b51401e76b4b6dbacca76198fd0f64c0?"
+                  className="w-4 aspect-square"
+                />
+                {getTranslation(
+                  `Home`, // -----> Anglais
+                  `Accueil` // -----> Français
+                )}
+              </a>
+            </div>
+
+            <div className="w-[100%] flex-1 flex justify-center separator hover-effect">
+              <a
+                className="flex gap-2 h-[100%] mr-[66px] my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn"
+                href={`/profile/${user?.id}`}
+              >
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/fc3698e77abea2c6c7f4158984e72cc84fccffa79005aaacb0a8148c7fc2d112?"
+                  className="aspect-[0.75] w-[13px]"
+                />
+                {getTranslation(
+                  `Profile`, // -----> Anglais
+                  `Profil` // -----> Français
+                )}
+              </a>
+            </div>
+
+            <div className="w-[100%] flex-1 flex justify-center separator hover-effect">
+              <a
+                className="flex gap-2 h-[100%] mr-4 my-auto text-base font-medium text-white whitespace-nowrap slideMenuBtn"
+                href={"/setting/personal"}
+              >
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/927661d7ec160577838c28e1e039b5d465c8c91f54895e62de4603b16322853d?"
+                  className="w-4 aspect-square"
+                />
+                {getTranslation(
+                  `Settings`, // -----> Anglais
+                  `Paramètres` // -----> Français
+                )}
+              </a>
+            </div>
+
+            <div
+              onClick={handleLogout}
+              className="flex gap-2 slideMenuBtn my-auto text-base font-medium text-white whitespace-nowrap  logout-hover-effect"
+            >
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7e89f8c7eb51a481e293bc932ceeb5782dea45f8469a1e16a1753ed7be9f6c3e?"
+                className="w-4 aspect-square"
+              />
+              <div>
+                {getTranslation(
+                  `Log Out`, // -----> Anglais
+                  `Déconnexion` // -----> Français
+                )}
+              </div>
+            </div>
+          </>
         </div>
       </div>
     </>
-  )
-}
-export default SlideMenu
+  );
+};
+export default SlideMenu;
