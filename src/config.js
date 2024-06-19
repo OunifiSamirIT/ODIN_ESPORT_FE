8c698561c008b6589b0aff2325677d9050d9bef5
Nader <ghazouani.nader@gmail.com>
2024-06-19 14:26:58 +0100

    fix config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const Config = {
-  LOCAL_URL: "http://192.168.100.21:5000",
-  // LOCAL_URL: "https://back.odinesport.com",
+  // LOCAL_URL: "http://localhost:5000",
+  LOCAL_URL: "https://back.odinesport.com",
 };
