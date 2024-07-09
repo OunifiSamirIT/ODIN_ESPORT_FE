f72059dcd67bb93e52d14053af901cdd3884aaee
Nader <ghazouani.nader@gmail.com>
2024-07-09 12:12:41 +0100

    fix width

--- src/components/HomePage/LeftMenuHomePage.jsx
+++ src/components/HomePage/LeftMenuHomePage.jsx
@@ -25,7 +25,7 @@ export default function LeftMenu({
           classothercomponent ? "mt-6" : ""
         } xs:hidden sm:hidden hidden  md:flex md:flex-col  md:min-w-[25%] md:-ml-2 md:mr-3 max-md:ml-0 max-md:w-full`}
       >
-        <div className=" fixed xs:hidden sm:hidden hidden max-h-[82vh] hiddenScrollLeftMenu overflow-y-scroll overflow-x-hidden md:flex md:flex-col md:max-w-[22%] max-md:ml-0 max-md:w-full ">
+        <div className=" fixed xs:hidden sm:hidden hidden max-h-[82vh] hiddenScrollLeftMenu overflow-y-scroll overflow-x-hidden md:flex md:flex-col md:max-w-[320px] max-md:ml-0 max-md:w-full ">
           <div className="flex flex-col max-w-full mr-4">
            <ChallengeSideBar/>
            <EventSideBar/>
