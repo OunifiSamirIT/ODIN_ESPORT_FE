b0ad5525998001ea12ac78f25e27633f809f0369
Dhia-dev <dhia.mejri@esprit.tn>
2024-08-27 16:28:44 +0100

    FIXX SECURITY

--- src/pages/Event/GallerieuserFormevent.jsx
+++ src/pages/Event/GallerieuserFormevent.jsx
@@ -106,6 +106,9 @@ const Album = () => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
   const [loadingForm, setLoadingForm] = useState(false);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
   const handleSubmit = async () => {
     try {
       // Validate form data
@@ -134,8 +137,6 @@ const Album = () => {
               emailsecondaire: formData.emailsecondaire,
               modepaiement: formData.modepaiement,
               email: storedUserData?.email,
-              // date_validation: formData.date_validation,
-              // fraisinscrit: formData.fraisinscrit,
               status: formData.status,
               champsoptionnel: formData.champsoptionnel,
               eventodinId: eventodinId,
@@ -146,23 +147,30 @@ const Album = () => {
             .then((response) => response.json())
             .then((data) => {
               setLoadingForm(false);
-              // Reset form data after successful submission
-              setFormData({
-                emailsecondaire: "",
-                modepaiement: "",
-                // date_validation: "",
-                // fraisinscrit: "",
-                status: "",
-                champsoptionnel: "",
-                eventodinId: "",
-                userId: "",
-              });
+              if (data.success) {
+                // Reset form data after successful submission
+                setFormData({
+                  emailsecondaire: "",
+                  modepaiement: "",
+                  status: "",
+                  champsoptionnel: "",
+                  eventodinId: "",
+                  userId: "",
+                });
 
-              navigate(`/thanksevent/${id}`);
-              console.log(handleSubmit, "aaloo");
+                navigate(`/thanksevent/${id}`);
+              } else if (data.message === "Invalid input detected.") {
+                setErrorMessage(
+                  "Invalid input detected. Please check your input."
+                );
+              } else {
+                setErrorMessage("An error occurred. Please try again.");
+              }
             })
             .catch((error) => {
               console.error("Error submitting form:", error);
+              setLoadingForm(false);
+              setErrorMessage("An error occurred. Please try again.");
             });
         })
         .catch((validationErrors) => {
@@ -174,18 +182,17 @@ const Album = () => {
               newErrors[error.path] = error.message;
             });
           } else {
-            // Handle the case where validationErrors.inner is not as expected
             console.error(
               "Unexpected validationErrors structure:",
               validationErrors
             );
-            // You might want to set a generic error message or handle this case differently
           }
 
           setErrors(newErrors);
         });
     } catch (error) {
       console.error("Error submitting form:", error);
+      setErrorMessage("An error occurred. Please try again.");
     }
   };
   const storedUserData = JSON.parse(secureLocalStorage.getItem("cryptedUser"));
@@ -463,6 +470,11 @@ const Album = () => {
                   onChange={handleChange}
                 />{" "}
               </div>
+              {errorMessage && (
+                <div className="text-red-500 text-center mt-4">
+                  {errorMessage}
+                </div>
+              )}
             </div>
           </div>
           <div className="flex gap-5 justify-between items-start mt-8 whitespace-nowrap max-md:flex-wrap max-md:max-w-full">
